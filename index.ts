8203c36e82f4206434ea43d941c1dfe4b75be40f
John Lorenz Inocentes <lorenzinocentes@gmail.com>
2026-02-10 01:51:52 +0800

    feat: enhance event handling and validation with zod schema

--- index.ts
+++ index.ts
@@ -1,11 +1,48 @@
-export const handler = async (event: unknown) => {
+import {  IssueEventSchema } from "./schemas";
+
+export const handler = async (event: any) => {
     console.log("Event received:", event);
+    if (!event.body) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: "Missing body" })
+        }
+    }
+
+
+    let body;
+    try {
+        body = JSON.parse(event.body);
+    } catch (e) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                error: "Invalid JSON payload"
+            })
+        }
+    };
+
+    const parsedEvent = IssueEventSchema.safeParse(body);
+
+    if (!parsedEvent.success) {
+        console.error("Validation Failed:", parsedEvent.error);
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                error: "Invalid payload",
+                details: parsedEvent.error.issues
+            }),
+        };
+    }
+
+    const validEvent = parsedEvent.data;
+
     return {
         statusCode: 200,
-        headers: {"Content-Type": "application/json"},
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
             status: 'alive',
-            message: "Smart hub is running"
+            message: `Processed issue: ${validEvent.issue.title}`,
         })
     }
 }
---
+++ schemas.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+
+export const IssueEventSchema = z.object({
+    action: z.literal("opened", "Only accepts opened events"),
+    issue: z.object({
+        url: z.string(),
+        html_url: z.string(),
+        id: z.number(),
+        title: z.string(),
+        user: z.object({
+            login: z.string(),
+            id: z.number()
+        }),
+        body: z.string().nullable().optional()
+    }),
+    repository: z.object({
+        id: z.number(),
+        full_name: z.string(),
+        private: z.boolean()
+    }),
+    sender: z.object({
+        login: z.string()
+    })
+});
+
+export type IssueEvent = z.infer<typeof IssueEventSchema>;
